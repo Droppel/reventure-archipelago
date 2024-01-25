ad0fdb628b40544bd8dcee2666e29d831919788e
DoppelRR <leagueoflegends@sofortstart.de>
2024-01-25 15:31:24 +0100

    Initial commit

---
+++ worlds/reventure/Items.py
@@ -0,0 +1,64 @@
+import typing
+
+from BaseClasses import Item
+from typing import Dict
+
+reventureOffset = 10000
+
+class ItemData(typing.NamedTuple):
+    code: typing.Optional[int]
+    progression: bool
+    hasWeight: bool = False
+
+item_table: Dict[str, ItemData] = {
+    #Item Locations
+    'SpawnSwordPedestalItem': ItemData(reventureOffset+1, True),
+    'SpawnSwordChest': ItemData(reventureOffset+2, True),
+    'SpawnShovelChest': ItemData(reventureOffset+3, True),
+    'SpawnBoomerang': ItemData(reventureOffset+4, True),
+    'SpawnMapChest': ItemData(reventureOffset+5, True),
+    'SpawnCompassChest': ItemData(reventureOffset+6, True),
+    'SpawnWhistleChest': ItemData(reventureOffset+7, True),
+    'SpawnBurgerChest': ItemData(reventureOffset+8, True),
+    'SpawnDarkstoneChest': ItemData(reventureOffset+9, True),
+    'SpawnHookChest': ItemData(reventureOffset+10, True),
+    'SpawnFishingRodChest': ItemData(reventureOffset+11, True),
+    'SpawnLavaTrinketChest': ItemData(reventureOffset+12, True),
+    'SpawnMrHugsChest': ItemData(reventureOffset+13, True),
+    'SpawnBombsChest': ItemData(reventureOffset+14, True),
+    'SpawnShieldChest': ItemData(reventureOffset+15, True),
+    'SpawnNukeItem': ItemData(reventureOffset+16, True),
+    'SpawnPrincessItem': ItemData(reventureOffset+17, True),
+    'SpawnAnvilItem': ItemData(reventureOffset+18, True),
+    'SpawnStrawberry': ItemData(reventureOffset+19, True),
+    
+    #Transportation/Paths
+    'UnlockShopCannon': ItemData(reventureOffset+20, True),
+    'UnlockCastleToShopCannon': ItemData(reventureOffset+21, True),
+    'UnlockDarkCastleCannon': ItemData(reventureOffset+22, True),
+    'UnlockCastleToDarkCastleCannon': ItemData(reventureOffset+23, True),
+    'UnlockGeyserDesert1': ItemData(reventureOffset+24, True),
+    'UnlockGeyserDesert2': ItemData(reventureOffset+25, True),
+    'UnlockGeyserVolcanoe': ItemData(reventureOffset+26, True), 
+    'UnlockGeyserWaterfall': ItemData(reventureOffset+27, True),
+    'UnlockElevatorButton': ItemData(reventureOffset+28, True),
+    'UnlockCallElevatorButtons': ItemData(reventureOffset+29, True),
+    'UnlockMirrorPortal': ItemData(reventureOffset+30, True),
+    'UnlockFairyPortal': ItemData(reventureOffset+31, True),
+    'GrowVine': ItemData(reventureOffset+32, True),
+    'OpenCastleFloor': ItemData(reventureOffset+33, True),
+    'UnlockFacePlantStone': ItemData(reventureOffset+34, True),
+
+    #NPCs
+    'SpawnDragon': ItemData(reventureOffset+35, True),
+    'SpawnShopkeeper': ItemData(reventureOffset+36, True),
+    'SpawnMimic': ItemData(reventureOffset+37, True),
+    'SpawnKing': ItemData(reventureOffset+38, True),
+
+    #Cosmetic
+    'EnableCloset': ItemData(reventureOffset+39, False),
+    'BuildStatue': ItemData(reventureOffset+40, True),
+    'AddPC': ItemData(reventureOffset+41, False),
+    'SpawnDolphins': ItemData(reventureOffset+42, False),
+    'SpawnMimicPet': ItemData(reventureOffset+43, False),
+}
---
+++ worlds/reventure/Rules.py
@@ -0,0 +1,167 @@
+from BaseClasses import MultiWorld
+from worlds.AutoWorld import LogicMixin
+from worlds.generic.Rules import set_rule
+
+
+class ReventureLogic(LogicMixin):
+    def _reventure_has_sword(self, player: int) -> bool:
+        return self.has_any(["SpawnSwordPedestalItem", "SpawnSwordChest"], player)
+    
+    def _reventure_has_nuke(self, player: int) -> bool:
+        return self.has_all(["SpawnNukeItem", "SpawnHookChest"], player)
+
+    def _reventure_has_weight(self, player: int, req: int) -> bool:
+        weightedItems = 0
+        if self._reventure_has_sword(player):
+            weightedItems += 1
+        if self.has("SpawnShovelChest", player):
+            weightedItems += 1
+        if self.has("SpawnShieldChest", player):
+            weightedItems += 1
+        if self.has("SpawnBombsChest", player):
+            weightedItems += 1
+        if self.has("SpawnHookChest", player):
+            weightedItems += 1
+        if self.has("SpawnLavaTrinketChest", player):
+            weightedItems += 1
+        if self.has("SpawnWhistleChest", player):
+            weightedItems += 1
+        return weightedItems >= req
+    
+    def _reventure_has_items(self, player: int, req: int) -> bool:
+        items = 0
+        if self._reventure_has_sword(player):
+            items += 1
+        if self.has("SpawnShovelChest", player):
+            items += 1
+        if self.has("SpawnShieldChest", player):
+            items += 1
+        if self.has("SpawnBombsChest", player):
+            items += 1
+        if self.has("SpawnHookChest", player):
+            items += 1
+        if self.has("SpawnLavaTrinketChest", player):
+            items += 1
+        if self.has("SpawnWhistleChest", player):
+            items += 1
+        if self.has("SpawnMrHugsChest", player):
+            items += 1
+        if self.has("SpawnMapChest", player):
+            items += 1
+        if self.has("SpawnCompassChest", player):
+            items += 1
+        return items >= req
+    
+    def _reventure_has_endings(self, world: MultiWorld, player: int, req: int) -> bool:
+        return world.get_reachable_locations(player=player) > req
+
+    def _reventure_can_reach_princessportal_with_item(self, player: int) -> bool:
+        return self.has("UnlockMirrorPortal", player) and (self._reventure_has_endings(player, 16) or self.has("GrowVine", player))
+
+    def _reventure_can_reach_princess_with_item(self, player: int) -> bool:
+        return self.has("SpawnPrincess", player) and (self._reventure_can_reach_princessportal_with_item(player) or self.has_any(["SpawnHookChest", "UnlockElevatorButton"], player))
+
+
+def set_rules(world: MultiWorld, p: int):
+    set_rule(world.get_location("StabElder", p), lambda state: state._reventure_has_sword(p))
+    set_rule(world.get_location("LonkFaceplant", p), lambda state: state.has("UnlockFacePlantStone", p))
+    set_rule(world.get_location("KilledByMinion", p), lambda state: True)
+    set_rule(world.get_location("StabGuard", p), lambda state: state._reventure_has_sword(p)),
+    set_rule(world.get_location("KillTheKing", p), lambda state: state._reventure_has_sword(p) and state.has("SpawnKing", p))
+    set_rule(world.get_location("FallIntoLava", p), lambda state: True)
+    set_rule(world.get_location("JumpIntoPiranhaLake", p), lambda state: True)
+    set_rule(world.get_location("WaterfallsBottom", p), lambda state: True)
+    set_rule(world.get_location("StabShopKeeper", p), lambda state: state._reventure_has_sword(p) and state.has("SpawnShopkeeper", p))
+    set_rule(world.get_location("DigIntoBottomlessPit", p), lambda state: state.has("SpawnShovelChest", p))
+    set_rule(world.get_location("RescueCat", p), lambda state: state.has("SpawnMrHugsChest", p))
+    set_rule(world.get_location("FindFishingRod", p), lambda state: state.has("SpawnFishingRodChest", p))
+    set_rule(world.get_location("HugMinion", p), lambda state: state.has("SpawnMrHugsChest", p))
+    set_rule(world.get_location("RoastedByDragon", p), lambda state: state.has("SpawnDragon", p))
+    set_rule(world.get_location("KilledByDarkArenaMinion", p), lambda state: True)
+    set_rule(world.get_location("StabDragon", p), lambda state: state._reventure_has_sword(p) and state.has("SpawnDragon", p))
+    set_rule(world.get_location("FaultyCannonShot", p), lambda state: state._reventure_has_endings(p, 15) and state.has_any(["UnlockShopCannon", "UnlockCastleToShopCannon", "UnlockDarkCastleCannon"], p))
+    set_rule(world.get_location("HugTheKing", p), lambda state: state.has_all(["SpawnMrHugsChest", "SpawnKing"], p))
+    set_rule(world.get_location("HugGuard", p), lambda state: state.has("SpawnMrHugsChest", p))
+    set_rule(world.get_location("TakeTheDayOff", p), lambda state: True)
+    set_rule(world.get_location("FallIntoSpikes", p), lambda state: True)
+    set_rule(world.get_location("PickupAnvil", p), lambda state: state.has("SpawnAnvilItem", p))
+    set_rule(world.get_location("EatenByFakePrincess", p), lambda state: state.has("SpawnMimic", p))
+    set_rule(world.get_location("ClimbMountain", p), lambda state: state.has("SpawnStrawberry", p))
+    set_rule(world.get_location("StabMinionMultipleTimes", p), lambda state: state._reventure_has_sword(p))
+    set_rule(world.get_location("CrushedByOwnStuff", p), lambda state: state._reventure_has_weight(p, 5))
+    set_rule(world.get_location("ShootCannonballToCastle", p), lambda state: state._reventure_has_nuke(p) and state.has("UnlockShopCannon", p))
+    set_rule(world.get_location("CaughtByOwnBomb", p), lambda state: state.has("SpawnBombsChest", p))
+    set_rule(world.get_location("DragonWithShield", p), lambda state: state.has_all(["SpawnDragon", "SpawnShieldChest"], p))
+    set_rule(world.get_location("EnterTheChimney", p), lambda state: True)
+    set_rule(world.get_location("DestroyAllPots", p), lambda state: state._reventure_has_sword(p))
+    set_rule(world.get_location("StabBoulder", p), lambda state: state._reventure_has_sword(p))
+    set_rule(world.get_location("LeapOfFaithFromTheMountain", p), lambda state: True)
+    set_rule(world.get_location("ElevatorCrush", p), lambda state: state.has_any(["UnlockElevatorButton", "UnlockCallElevatorButtons"], p))
+    set_rule(world.get_location("GetIntoThePipe", p), lambda state: True)
+    set_rule(world.get_location("DragonWithFireTrinket", p), lambda state: state.has_all(["SpawnDragon", "SpawnLavaTrinketChest"], p))
+    set_rule(world.get_location("HugShopkeeper", p), lambda state: state.has_all(["SpawnShopkeeper", "SpawnMrHugsChest"], p))
+    set_rule(world.get_location("WrongLever", p), lambda state: True)
+    set_rule(world.get_location("GetIntoBigChest", p), lambda state: True)
+    set_rule(world.get_location("HugElder", p), lambda state: state.has("SpawnMrHugsChest", p))
+    set_rule(world.get_location("DragonWithShieldAndFireTrinket", p), lambda state: state.has_all(["SpawnDragon", "SpawnLavaTrinketChest", "SpawnShieldChest"], p))
+    set_rule(world.get_location("AirDuctsAccident", p), lambda state: True)
+    set_rule(world.get_location("HugDragon", p), lambda state: state.has_all(["SpawnDragon", "SpawnMrHugsChest"], p))
+    set_rule(world.get_location("WrongLever2", p), lambda state: True)
+    set_rule(world.get_location("TakePrincessToBed", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("JumpOffTheCliff", p), lambda state: True)
+    set_rule(world.get_location("HarakiriSuicide", p), lambda state: state._reventure_has_sword(p))
+    set_rule(world.get_location("SelfDestructFortress", p), lambda state: True)
+    set_rule(world.get_location("HundredMinionsMassacre", p), lambda state: state._reventure_has_sword(p))
+    set_rule(world.get_location("KilledByDarkLord", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("TakePrincessBackToTown", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("ShootPrincessToTown", p), lambda state: state.has_all(["SpawnPrincessItem", "UnlockDarkCastleCannon"], p))
+    set_rule(world.get_location("FallWithPrincessToAnvilPit", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("HugBoulder", p), lambda state: state.has("SpawnMrHugsChest", p))
+    set_rule(world.get_location("JumpOffTheBalconyWithPrincess", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("ShootCannonballToShop", p), lambda state: state._reventure_has_nuke(p) and state.has("UnlockCastleToShopCannon", p))
+    set_rule(world.get_location("HugPrincess", p), lambda state: state.has_all(["SpawnMrHugsChest", "SpawnPrincessItem"], p))
+    set_rule(world.get_location("JumpOffTheBalcony", p), lambda state: True)
+    set_rule(world.get_location("StayAfk", p), lambda state: True)
+    set_rule(world.get_location("PlaceBombUnderCastle", p), lambda state: state.has("SpawnBombsChest", p))
+    set_rule(world.get_location("DontKillMinions", p), lambda state: True)
+    set_rule(world.get_location("FindTreasure", p), lambda state: state.has("SpawnShovelChest", p))
+    set_rule(world.get_location("KillChicken", p), lambda state: state._reventure_has_sword(p) and state._reventure_has_endings(p, 16))
+    set_rule(world.get_location("StabPrincess", p), lambda state: state._reventure_has_sword(p) and state._reventure_can_reach_princess_with_item(p))
+    set_rule(world.get_location("OverhealByFairies", p), lambda state: True)
+    set_rule(world.get_location("DarkStoneToAltar", p), lambda state: state.has("SpawnDarkstoneChest", p))
+    set_rule(world.get_location("CrushedAtUltimateDoor", p), lambda state: state._reventure_has_endings(p, 6))
+    set_rule(world.get_location("DarkLordComicStash", p), lambda state: True)
+    set_rule(world.get_location("StabDarkLord", p), lambda state: state._reventure_has_sword(p) and state._reventure_can_reach_princess_with_item(p))
+    set_rule(world.get_location("TriggerTrollSpikes", p), lambda state: True)
+    set_rule(world.get_location("SacrificeEveryItem", p), lambda state: state._reventure_has_items(p, 6))
+    set_rule(world.get_location("SacrificePrincess", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("HugDarkLord", p), lambda state: state.has_all(["SpawnMrHugsChest", "SpawnPrincessItem"], p))
+    set_rule(world.get_location("ShotgunFakePrincess", p), lambda state: state._reventure_has_sword(p) and state.has_all(["SpawnShopkeeper", "SpawnMimic", "UnlockShopCannon", "UnlockElevatorButton"], p))
+    set_rule(world.get_location("FakePrincessInsideChest", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("TakePrincessToDarkAltar", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("GetIntoTheCloud", p), lambda state: state._reventure_has_endings(p, 16) and state.has("GrowVine", p))
+    set_rule(world.get_location("KidnapPrincess", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("HugChicken", p), lambda state: state._reventure_has_endings(p, 16) and state.has("SpawnMrHugsChest", p))
+    set_rule(world.get_location("TakeChickenToDarkAltar", p), lambda state: state._reventure_has_endings(p, 16))
+    set_rule(world.get_location("PrincessToDesertGate", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("FallIntoWaterfallWithPrincess", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("BreakSpaceTimeContinuum", p), lambda state: state.has("SpawnWhistleChest", p))
+    set_rule(world.get_location("ShootCannonballToTown", p), lambda state: state._reventure_has_nuke and state.has("UnlockDarkCastleCannon", p))
+    set_rule(world.get_location("KillAllFairies", p), lambda state: state._reventure_has_sword(p) or state.has("SpawnMrHugsChest", p) or (state.has("SpawnBoomerang", p) and (state.has("SpawnHookChest", p) or state._reventure_has_endings(p, 16))))
+    set_rule(world.get_location("MakeBabiesWithPrincess", p), lambda state: state._reventure_has_sword(p) and state._reventure_can_reach_princess_with_item(p))
+    set_rule(world.get_location("KillAllDevsHell", p), lambda state: state._reventure_has_sword(p) and state.has("SpawnPrincessItem", p) and (state.has("SpawnHookChest", p) or (state.has("SpawnShovelChest", p) and state._reventure_has_endings(p, 16) and (state.has("UnlockElevatorButton", p) or state._reventure_can_reach_princessportal_with_item(p)))))
+    set_rule(world.get_location("DesertEnd", p), lambda state: state._reventure_has_weight(p, 4) and state.has("UnlockGeyserDesert2", p))
+    set_rule(world.get_location("FindAlienLarvae", p), lambda state: state.has("SpawnShovelChest", p))
+    set_rule(world.get_location("FaceDarkLordWithShield", p), lambda state: state.has_all(["SpawnShieldChest", "SpawnPrincessItem"], p) and (state._reventure_can_reach_princessportal_with_item(p) or (state.has_any(["SpawnHookChest", "UnlockElevatorButton"], p) and (state._reventure_has_sword(p) or state._reventure_has_endings(p, 16) or state.has_any(["SpawnShovelChest", "UnlockShopCannon", "UnlockGeyserVolcanoe"], p)))))
+    set_rule(world.get_location("MultipleDesertJumps", p), lambda state: state._reventure_has_weight(p, 4))
+    set_rule(world.get_location("DatePrincessAndDragon", p), lambda state: state.has_all(["SpawnPrincessItem", "SpawnDragon"], p))
+    set_rule(world.get_location("GiveDarkStoneToDarkLord", p), lambda state: state.has("SpawnDarkstoneChest", p) and state._reventure_can_reach_princess_with_item(p))
+    set_rule(world.get_location("TakePrincessToLonksHouse", p), lambda state: state.has("SpawnPrincessItem", p))
+    set_rule(world.get_location("StayInTheWater", p), lambda state: True)
+    set_rule(world.get_location("AboardPirateShip", p), lambda state: True)
+    set_rule(world.get_location("SwimIntoTheOcean", p), lambda state: True)
+    set_rule(world.get_location("FeedTheMimic", p), lambda state: state.has_all(["SpawnBurgerChest", "SpawnMimic"], p) and (state._reventure_can_reach_princessportal_with_item(p) or state.has_any(["SpawnHookChest", "UnlockElevatorButton"], p)))
+    set_rule(world.get_location("FeedTheKing", p), lambda state: state.has_all(["SpawnBurgerChest", "SpawnKing"], p))
+    set_rule(world.get_location("UltimateEnding", p), lambda state: state._reventure_has_endings(p, 99) and state._reventure_has_items(p, 4))
+
+    world.completion_condition[p] = lambda state: state._reventure_has_endings(p, world.endings[p])
