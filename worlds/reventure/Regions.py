b1aca96c3faa1e390b0e5cc07f673278bf611807
DoppelRR <leagueoflegends@sofortstart.de>
2024-02-24 14:34:17 +0100

    Added ending number to all locations

--- worlds/reventure/Regions.py
+++ worlds/reventure/Regions.py
@@ -1,11 +1,13 @@
-def create_regions(world, player: int):
+from BaseClasses import MultiWorld
+
+def create_regions(world: MultiWorld, player: int):
     from . import create_region
     from .Locations import location_table
 
     world.regions += [
-        create_region(world, player, 'Menu', None, ['Lonk\'s House']),
+        create_region(world, player, 'Menu', None, ['Startbutton']),
         create_region(world, player, 'Reventureworld', [location for location in location_table])
     ]
 
     # link up our region with the entrance we just made
-    world.get_entrance('Lonk\'s House', player).connect(world.get_region('Reventureworld', player))
+    world.get_entrance('Startbutton', player).connect(world.get_region('Reventureworld', player))
